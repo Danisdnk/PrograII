2f786353b7f44a4706be7fb49e948e5a9ce9a8a8
danisdnk <danisdnk@gmail.com>
2021-08-24 02:40:56 -0300

    c) Invertir el contenido de una Cola (NO pueden usarse Pilas auxiliares) falta

--- src/main/TP1/ColasCuatroC.java
+++ src/main/TP1/ColasCuatroC.java
@@ -1,27 +1,24 @@
 package main.TP1;
 
 import impl.Colas.Estaticas.ColaTF1;
-import impl.Pilas.Estaticas.PilaTF1;
+import impl.Colas.Estaticas.ColaTF3;
+
 
 public class ColasCuatroC {
-    /** c) Invertir el contenido de una Cola (NO pueden usarse Pilas auxiliares)
-    d) Determinar si el final de la Cola C1 coincide o no con la Cola C2.
-            e) Determinar si una Cola es capicúa o no. Para ser capicúa debe cumplir
-    que el primer elemento es igual al último, el segundo igual al penúltimo, etc.
-    f) Determinar si la Cola C1 es la inversa de la Cola C2. Dos Colas serán
-    inversas, si tienen los mismos elementos pero en orden inverso.**/
+    // c) Invertir el contenido de una Cola (NO pueden usarse Pilas auxiliares)
 
-    public static ColaTF1 cargarCola() {
-        ColaTF1 cola = new ColaTF1();
+    public static ColaTF3 cargarCola() {
+        ColaTF3 cola = new ColaTF3();
         cola.InicializarCola();
         cola.Acolar(3);
         cola.Acolar(45);
         cola.Acolar(76);
+        cola.Acolar(90);
 
         return cola;
     }
 
-    public static ColaTF1 pasarCola(ColaTF1 cola) {
+    public static ColaTF1 pasarCola(ColaTF3 cola) {
 
         ColaTF1 copia = new ColaTF1();
         copia.InicializarCola();
@@ -29,9 +26,8 @@ public class ColasCuatroC {
         while (!cola.ColaVacia()) {
             copia.Acolar(cola.Primero());
             cola.Desacolar();
+
         }
-
-
         return copia;
     }
 
@@ -41,7 +37,6 @@ public class ColasCuatroC {
         while (!cola.ColaVacia()) {
             System.out.println(cola.Primero());
             cola.Desacolar();
-
         }
     }
 
